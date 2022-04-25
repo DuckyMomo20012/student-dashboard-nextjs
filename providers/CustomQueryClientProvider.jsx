ce85840ad08a9c75fb2c682346a619e8748c9d4f
19127631_Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-04-25 22:51:15 +0700

    chore: change react-query devtool position

--- providers/CustomQueryClientProvider.jsx
+++ providers/CustomQueryClientProvider.jsx
@@ -8,7 +8,7 @@ const CustomQueryClientProvider = ({ children }) => {
     <QueryClientProvider client={queryClient}>
       <>
         {children}
-        <ReactQueryDevtools initialIsOpen={false} />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       </>
     </QueryClientProvider>
   );
