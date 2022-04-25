008870645c9a1cad41380e91cf0cb035dcc559fb
19127631_Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-04-25 23:37:21 +0700

    feat: add loading indicator when redirecting

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,16 +4,17 @@ import {
   CustomReduxProvider,
 } from '@provider/index.js';
 import { SessionProvider } from 'next-auth/react';
+import NextNProgress from 'nextjs-progressbar';
 import 'windi.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const getLayout = Component.getLayout || ((page) => page);
-
   return getLayout(
     <CustomReduxProvider>
       <SessionProvider session={session}>
         <CustomQueryClientProvider>
           <CustomMantineProvider>
+            <NextNProgress />
             <Component {...pageProps} />
           </CustomMantineProvider>
         </CustomQueryClientProvider>
