00a1ec1d67ba981c2e079ef736dda728ee00fe83
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-05-05 17:40:29 +0700

    fix: header menu not open

--- src/components/elements/DataGrid/Header/Label.jsx
+++ src/components/elements/DataGrid/Header/Label.jsx
@@ -3,17 +3,17 @@ import { Center, Group, Text } from '@mantine/core';
 import { Icon } from '@iconify/react';
 import { forwardRef } from 'react';
 
-// NOTE: Remember to remove unnecessary props when destructuring, or menu won't
-// open popper
+// NOTE: Remember to put 'others' as the last props, because it has onClick and
+// onKeyDown props which maybe overridden -> popper won't open.
 // eslint-disable-next-line react/display-name
 const HeaderLabel = forwardRef(
   ({ color, label, icon, isLabelHidden, onAddColumnClick, ...others }, ref) => {
     return (
       <Center
         className={`h-full rounded-md hover:bg-${color}-50 px-2 py-1`}
+        onClick={onAddColumnClick}
         ref={ref}
         {...others}
-        onClick={onAddColumnClick}
       >
         <Group
           className={`w-full text-${color}-700 flex-nowrap items-center`}
