aa58e8990402ed912a364c43537fce231d0c34c2
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-05-20 09:57:28 +0700

    style: format files
    chore(data): add data for SQL Server

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,6 @@
-import { createGetInitialProps } from '@mantine/next';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+import { createGetInitialProps } from '@mantine/next';
 
 export default class _Document extends Document {
   static getInitialProps = createGetInitialProps();
